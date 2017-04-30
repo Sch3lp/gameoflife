29f89a22dd3c9449026601ea40aa6283918b7eab
Tim Schraepen <sch3lp@gmail.com>
2017-04-30 18:18:37 +0200

    implemented underpopulation rule

--- src/main/java/be/cegeka/gameoflife/UnderpopulationRule.java
+++ src/main/java/be/cegeka/gameoflife/UnderpopulationRule.java
@@ -3,7 +3,10 @@ package be.cegeka.gameoflife;
 import java.util.List;
 
 public class UnderpopulationRule {
-    public String apply(Cell cell, List<Cell> liveNeighbours){
-        return "";
+
+    public String apply(Cell cell, List<Cell> liveNeighbours) {
+        if (cell.isDead()) return "";
+        if (liveNeighbours.size() >= 2) return "";
+        return "Death";
     }
 }

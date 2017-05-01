10338fe4752078a1cd0d2131c7285a9ec59ef135
Tim Schraepen <sch3lp@gmail.com>
2017-05-01 13:49:18 +0200

    modified UnderpopulationRuleTest to use TestBuilder

--- src/test/java/be/cegeka/gameoflife/domain/rules/UnderpopulationRuleTest.java
+++ src/test/java/be/cegeka/gameoflife/domain/rules/UnderpopulationRuleTest.java
@@ -1,85 +1,68 @@
 package be.cegeka.gameoflife.domain.rules;
 
-import be.cegeka.gameoflife.domain.Cell;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-
-import static be.cegeka.gameoflife.domain.Cell.dead;
-import static be.cegeka.gameoflife.domain.Cell.live;
+import static be.cegeka.gameoflife.domain.rules.RuleCellTestBuilder.aDeadRuleCell;
 import static be.cegeka.gameoflife.domain.rules.RuleCellTestBuilder.aLiveRuleCell;
 import static be.cegeka.gameoflife.domain.rules.UnderpopulationRule.DEATH;
 import static be.cegeka.gameoflife.domain.rules.UnderpopulationRule.NOTHING;
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnderpopulationRuleTest {
 
+    private UnderpopulationRule underpopulationRule;
+
+    @Before
+    public void setUp() throws Exception {
+        underpopulationRule = new UnderpopulationRule();
+    }
+
     @Test
     public void apply_LiveCellWithoutNeighbours_Death() throws Exception {
         RuleCell cell = aLiveRuleCell().withoutNeighbours().build();
-        String outcome = new UnderpopulationRule().apply(cell);
+        String outcome = underpopulationRule.apply(cell);
 
         assertThat(outcome).isEqualTo(DEATH);
     }
 
     @Test
     public void apply_LiveCellWithExactlyOneNeighbour_Death() throws Exception {
-        Cell liveCell = live();
-        List<Cell> liveNeighbours = asList(live());
-
-        RuleCell ruleCell = createRuleCell(liveCell, liveNeighbours);
-        String outcome = new UnderpopulationRule().apply(ruleCell);
+        RuleCell ruleCell = aLiveRuleCell().withAmountOfNeighbours(1).build();
+        String outcome = underpopulationRule.apply(ruleCell);
 
         assertThat(outcome).isEqualTo(DEATH);
     }
 
     @Test
     public void apply_LiveCellWithExactlyTwoNeighbours_Nothing() throws Exception {
-        Cell liveCell = live();
-        List<Cell> liveNeighbours = asList(live(), live());
-
-        RuleCell ruleCell = createRuleCell(liveCell, liveNeighbours);
-        String outcome = new UnderpopulationRule().apply(ruleCell);
+        RuleCell ruleCell = aLiveRuleCell().withAmountOfNeighbours(2).build();
+        String outcome = underpopulationRule.apply(ruleCell);
 
         assertThat(outcome).isEqualTo(NOTHING);
     }
 
     @Test
     public void apply_DeadCellWithoutNeighbours_Nothing() throws Exception {
-        Cell deadCell = dead();
-        List<Cell> liveNeighbours = emptyList();
-
-        RuleCell ruleCell = createRuleCell(deadCell, liveNeighbours);
-        String outcome = new UnderpopulationRule().apply(ruleCell);
+        RuleCell ruleCell = aDeadRuleCell().withoutNeighbours().build();
+        String outcome = underpopulationRule.apply(ruleCell);
 
         assertThat(outcome).isEqualTo(NOTHING);
     }
 
     @Test
     public void apply_DeadCellWithExactlyOneNeighbour_Nothing() throws Exception {
-        Cell deadCell = dead();
-        List<Cell> liveNeighbours = asList(live());
-
-        RuleCell ruleCell = createRuleCell(deadCell, liveNeighbours);
-        String outcome = new UnderpopulationRule().apply(ruleCell);
+        RuleCell ruleCell = aDeadRuleCell().withAmountOfNeighbours(1).build();
+        String outcome = underpopulationRule.apply(ruleCell);
 
         assertThat(outcome).isEqualTo(NOTHING);
     }
 
     @Test
     public void apply_DeadCellWithExactlyTwoNeighbours_Nothing() throws Exception {
-        Cell deadCell = dead();
-        List<Cell> liveNeighbours = asList(live(), live());
-
-        RuleCell ruleCell = createRuleCell(deadCell, liveNeighbours);
-        String outcome = new UnderpopulationRule().apply(ruleCell);
+        RuleCell ruleCell = aDeadRuleCell().withAmountOfNeighbours(2).build();
+        String outcome = underpopulationRule.apply(ruleCell);
 
         assertThat(outcome).isEqualTo(NOTHING);
-    }
-
-    private RuleCell createRuleCell(Cell liveCell, List<Cell> liveNeighbours) {
-        return new RuleCellFactory().createRuleCell(liveCell, liveNeighbours);
     }
 }
